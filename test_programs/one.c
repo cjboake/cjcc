5722feed6d4112c46981cadd30555ff6cd290671
ParmesanB <parmesanb@gmail.com>
2018-05-21 19:38:21 -0400

    added test programs and makefile

---
+++ test_programs/one.c
@@ -0,0 +1,11 @@
+// An outrageously simple C program that we hope to compile with Babyc.
+//
+// To test with GCC:
+//   $ gcc -Wall one.c -o one
+//   $ ./one
+//   $ echo $?
+//   1
+
+int main() {
+    return 1;
+}
---
+++ test_programs/test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void printer()
+{
+    printf("Hello, you fucks\n");
+}
